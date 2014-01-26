f6638b9b86e2236f697b3cc319854f1559501834
Barry Rowlingson <b.rowlingson@gmail.com>
2014-01-26 19:43:10 +0000

    fleshing out

--- project/ahha/views.py
+++ project/ahha/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
 
 # Create your views here.
+
+def index(request):
+    return render_to_response("index.html",{}, 
+                              context_instance=RequestContext(request))
