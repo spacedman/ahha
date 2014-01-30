232927a55e0b043ed524aa7b76d16163f0044cce
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2014-01-30 16:57:22 +0000

    tweaked model, added dummy data

--- base/models.py
+++ base/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class DailyRecord(models.Model):
-      date = models.DateField(blank=True, unique=True)
-      predictionTotal = models.FloatField(blank=True)
-      surgicalAdmissions = models.PositiveIntegerField(blank=True)
-      medicalAdmissions = models.PositiveIntegerField(blank=True)
-      surgicalTCI = models.PositiveIntegerField(blank=True)
-      medicalTCI = models.PositiveIntegerField(blank=True)
+      date = models.DateField(unique=True)
+      predictionTotal = models.FloatField(blank=True, null=True)
+      surgicalAdmissions = models.PositiveIntegerField(blank=True, null=True)
+      medicalAdmissions = models.PositiveIntegerField(blank=True, null=True)
+      surgicalTCI = models.PositiveIntegerField(blank=True, null=True)
+      medicalTCI = models.PositiveIntegerField(blank=True, null=True)
       
       def __unicode__(self):
-            return u"Record for $s" % (self.date)
+            return u"Record for %s" % (self.date)
