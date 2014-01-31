cc431b94f0b650d992362ae1be578277494dc606
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2014-01-31 19:44:31 +0000

    model now has separate med/surg for all items

--- base/models.py
+++ base/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class DailyRecord(models.Model):
       date = models.DateField(unique=True)
-      predictionTotal = models.FloatField(blank=True, null=True)
-      surgicalAdmissions = models.PositiveIntegerField(blank=True, null=True)
-      medicalAdmissions = models.PositiveIntegerField(blank=True, null=True)
-      surgicalTCI = models.PositiveIntegerField(blank=True, null=True)
-      medicalTCI = models.PositiveIntegerField(blank=True, null=True)
+      MedActual = models.PositiveIntegerField(blank=True, null=True)
+      SurgActual = models.PositiveIntegerField(blank=True, null=True)
+      MedPredict = models.FloatField(blank=True, null=True)
+      SurgPredict = models.FloatField(blank=True, null=True)
+      MedTCI = models.PositiveIntegerField(blank=True, null=True)
+      SurgTCI = models.PositiveIntegerField(blank=True, null=True)
       
       def __unicode__(self):
             return u"Record for %s" % (self.date)
