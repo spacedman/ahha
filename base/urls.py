f2d535f615dfc7e9ae29abf77bdb5aec77513981
Barry Rowlingson <b.rowlingson@gmail.com>
2014-02-01 16:52:05 +0000

    added json response

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('base.views',
     url(r'^$', 'home', name='home'),
     url(r'^login$', 'login', name='login'),
     url(r'^logout$', 'logout', name='logout'),
+    url(r'^logout$', 'logout', name='logout'),
+    url(r'^api/hospital/(?P<year>\d{4})/(?P<month>\d{2})/$','hospital',name="jospital"),
 )
--- base/views.py
+++ base/views.py
@@ -2,7 +2,13 @@
 
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+from django.http import HttpResponse
+
+from jsonview.decorators import json_view
+
+from . import models
 
 def home(request):
     """ Default view for the root """
@@ -29,4 +35,16 @@ def login(request):
 def logout(request):
     logout(request)
     return HttpResponseRedirect(".")
+
+@login_required
+@json_view
+def hospital(request, year, month):
+    records = models.DailyRecord.objects.filter(date__month=month, date__year=year)
+    MedActual = [record.MedActual for record in records]
+    SurgActual = [record.SurgActual for record in records]
+    #return render(request, 'base/home.html')
+    return {
+        'date': [str(record.date) for record in records],
+        'MedActual': MedActual,
+        'SurgActual': SurgActual}
 
